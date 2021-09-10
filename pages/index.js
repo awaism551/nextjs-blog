1be50492c307ebe00c9ab8341dc6dbf5f9b6bdd8
Muhammad Awais <muhammad.awais4@afiniti.com>
2021-09-10 20:57:31 +0500

    some testing code added

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,10 @@ export default function Home({ allPostsData }) {
         <p>I am a Software Engineer with having market experience still 2017, working in Afiniti these days as a Full Stack Engineer.</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          <Link href="/next-tutorial">
+            <a>Our Nextjs tutorial</a>
+          </Link>
+          .)
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
---
+++ posts/third-post.md
@@ -0,0 +1,6 @@
+---
+title: '3rd post'
+date: '2020-01-01'
+---
+
+test testing test
